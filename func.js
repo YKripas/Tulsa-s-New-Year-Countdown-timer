d236845216ccf24363858bba9fdeef7ef291925a
Juozas Kripas <2005.joe@gmail.com>
2016-04-05 10:30:41 -0600

    HTML Creating NY Clock function for placeholding

---
+++ func.js
@@ -0,0 +1,21 @@
+/*
+   New Perspectives on HTML, CSS, and JavaScript
+   Tutorial 11
+   Tutorial Case
+
+   Author: Juozas Kripas
+   Date:   4/5/2016
+
+   Function List:
+   showDate(dateObj)
+      Returns the current date in the format mm/dd/yyyy
+
+   showTime(dateObj)
+      Returns the current time in the format hh:mm:ss a.m./p.m.
+
+   calcDays(currentDate)
+      Returns the number of days between the current date and January 1st
+      of the next year
+
+*/
+
